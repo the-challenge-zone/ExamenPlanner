e089bb0c703ca62bcc3a7dd4489720f3f685246d
JulienBasters <56169949+JulienBasters@users.noreply.github.com>
2022-04-01 12:00:20 +0200

    ja dat ie het nu wel doet gvd

--- bewerken.php
+++ bewerken.php
@@ -1,12 +1,12 @@
 <?php
 // Initialize the session
-session_start();
+//session_start();
  
-// Check if the user is logged in, if not then redirect them to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: index.php");
-    exit;
-}
+// Check if the user is logged in, if not then redirect him to login page
+//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//    header("location: index.php");
+//    exit;
+//}
 ?>
 
 <!DOCTYPE html>
@@ -23,37 +23,200 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <nav class="navbar navbar-expand navbar-light bg-dark sticky-top"> <!--Start navbar-->
         <div class="container"><!--Start container-->
-            <button id="menu-toggle" class="btn btn-danger"><?php echo htmlspecialchars($_SESSION["username"]); ?></button> <!--Login button-->
-            <a class="nav-link" href="index.php" style="color: #ffffff;">Examen Planner</a> <!--Home button-->
+            <button id="menu-toggle" class="btn btn-danger"><?php //echo htmlspecialchars($_SESSION["username"]); ?></button> <!--Login button-->
+            <a class="nav-link" href="test.php" style="color: #ffffff;">Examen Planner</a> <!--Home button-->
             <a class="nav-link" href="#" style="color: #ffffff; border-bottom: 2px solid #d8c0fd;">Bewerken</a> <!--Klas rankings button-->
         </div> <!--End container-->
     </nav> <!--End navbar-->
-    <div id="wrapper"> <!--Start wrapper-->
-        <div id="sidebar-wrapper"> <!--Start sidebar wrapper-->
-            <div class="container"> <!--Start container-->
-                <ul class="sidebar-nav"> <!--Start sidebar-->
-                <?php 
-                if(!empty($login_err)){
-                    echo '<div class="alert alert-danger">' . $login_err . '</div>';
-                }        
-                ?>
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                    <div class="form-group">
-                        <label>Username</label>
-                        <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>" placeholder="username">
-                        <span class="invalid-feedback"><?php echo $username_err; ?></span>
-                    </div>    
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" placeholder="password">
-                        <span class="invalid-feedback"><?php echo $password_err; ?></span>
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Login</button>
-                    </div>
-                </form>
-                </ul> <!--End sidebar-->
-            </div> <!--End container-->
-        </div> <!--End sidebar wrapper-->
+    
+    <form class="form-horizontal">
+        <fieldset>
+
+        <!-- Form Name -->
+        <legend style="text-align:center;">Examen planner formulier</legend>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput" style = "margin-left: 45%">Examen naam</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md" style = "margin-left: 105%">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput"  style = "margin-left: 45%">Klas</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md"  style = "margin-left: 105%">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput"  style = "margin-left: 45%">Crebo</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md"  style = "margin-left: 105%">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput"  style = "margin-left: 45%">Kwalificatie</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md"  style = "margin-left: 105%">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput"  style = "margin-left: 45%">Cohort</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md"  style = "margin-left: 105%">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput"  style = "margin-left: 45%">Benaming exameneenheid</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md"  style = "margin-left: 105%">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput"  style = "margin-left: 45%">Code exameneenheid</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md"  style = "margin-left: 105%">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Afname vorm</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Duur (min)</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Dag</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Datum -tijd v/t of v</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">beoordelaar</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Leverancier </label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Toezicht- lokaal</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Aantal</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Studentnummers</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Cluster</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Versie</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Week</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Locatie opmerking</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+            
+        </div>
+        </div>
+
+        </fieldset>
+    </form>
+
 </body>
 </html>
