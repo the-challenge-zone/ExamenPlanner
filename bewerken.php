7e39305b46888c565b6138d6c257f86b6a8d21d0
Miirari <sshadowofficial@gmail.com>
2022-04-08 09:39:08 +0200

    Update bewerken.php

--- bewerken.php
+++ bewerken.php
@@ -1,12 +1,12 @@
 <?php
 // Initialize the session
-//session_start();
+session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
-//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-//    header("location: index.php");
-//    exit; 
-//}
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit; 
+}
 
 // Including the configuration
 require_once 'config.php';
