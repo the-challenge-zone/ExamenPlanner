abf48c547eefa1a4a9ad098d24579ce8824992d5
JulienBasters <56169949+JulienBasters@users.noreply.github.com>
2022-04-14 14:30:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,11 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
  
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
- 
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password FROM user WHERE username = ?";
         
         if($stmt = $mysqli->prepare($sql)){
             // Bind variables to the prepared statement as parameters
@@ -141,8 +141,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 </div>
                 <input id="search" type="text" class="form-control" aria-label="Search" aria-describedby="basic-addon1">
             </div>
-            <div id="calendar"></div>
-                <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min.js"></script>
+            <table class="table table-hover table-dark">
+                <tr><th>April</th></tr>
+                <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>
+                <tr><td>8</td><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td></tr>
+                <tr><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td></tr>
+                <tr><td>22</td><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td>28</td></tr>
+                <tr><td>29</td><td>30</td></tr>
+            </table>
             <br>
         </div>
     <script src="./js/jquery.js"></script>
